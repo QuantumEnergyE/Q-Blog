5e43869bb5b7cf9fc8832ee2e937e5a5691a735c
QuantumEnergyE <quantumenergye@gmail.com>
2018-03-07 22:29:28 +0800

    update to wagtail 2.0

--- blog/blog/settings/dev.py
+++ blog/blog/settings/dev.py
@@ -1,12 +1,10 @@
-from __future__ import absolute_import, unicode_literals
-
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yo=s^8^bq*46b$)6sc!g&jhdv9q(p6fu35bwic#()&+-f6*)@&'
+SECRET_KEY = 'h53d_#wpt0cb_!w(qy@3!*vysgme%o74s!u)ep0ndq3^qp0wv*'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

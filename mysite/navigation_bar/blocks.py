9e23c32f3b697b2e5e2b43c4e1880052f7200f7b
QuantumEnergyE <quantumenergye@gmail.com>
2018-03-11 22:07:20 +0800

    add navigation bar

---
+++ mysite/navigation_bar/blocks.py
@@ -0,0 +1,49 @@
+from wagtail.core import blocks
+
+class BaseLinkBlock(blocks.StructBlock):
+    """
+    Base StructBlock class used to prevent DRY code.
+    """
+    display_text = blocks.CharBlock()
+
+
+class ExternalLinkBlock(BaseLinkBlock):
+    """
+    Block that holds a link to any URL.
+    """
+    link = blocks.URLBlock()
+
+    class Meta:
+        template = 'navigation_bar/external_link_block.html'
+
+class PageLinkBlock(BaseLinkBlock):
+    """
+    Block that holds a page.
+    """
+    page = blocks.PageChooserBlock()
+
+    class Meta:
+        template = 'navigation_bar/page_link_block.html'
+
+class LinkChildrenBlock(blocks.StructBlock):
+    """
+    Base childblock for second level children.
+    """
+    children = blocks.StreamBlock(
+            [
+                ('external_link', ExternalLinkBlock()),
+                ('page_link', PageLinkBlock()),
+            ]
+        )
+
+class ExternalLinkWithChildrenBlock(LinkChildrenBlock, ExternalLinkBlock):
+    """
+    Uses LinkChildrenBlock as a mixin to create an ExternalLinkBlock that supports Children.
+    """
+    pass
+
+class PageLinkWithChildrenBlock(LinkChildrenBlock, PageLinkBlock):
+    """
+    Uses LinkChildrenBlock as a mixin to create a PageLinkBlock that supports Children.
+    """
+    pass
---
+++ mysite/navigation_bar/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from .blocks import ExternalLinkWithChildrenBlock, PageLinkWithChildrenBlock
+
+class Navbar(models.Model):
+    """
+    Model that represents website navigation bars.  Can be modified through the
+    snippets UI.
+    """
+    name = models.CharField(max_length=255)
+    menu_items = StreamField([
+        ('external_link', ExternalLinkWithChildrenBlock()),
+        ('page_link', PageLinkWithChildrenBlock()),
+        ],)
+
+    panels = [
+        FieldPanel('name'),
+        StreamFieldPanel('menu_items')
+    ]
+
+    def __str__(self):
+        return self.name
+
+register_snippet(Navbar)
+
+
+class PageWithNavbar(Page):
+    navbar = models.ForeignKey(
+        Navbar,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    content_panels=[
+        FieldPanel('title'),
+        SnippetChooserPanel('navbar')
+    ]
